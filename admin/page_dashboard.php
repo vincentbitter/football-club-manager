1723984d8ebd47a2f2beb2c3a4f334bbf6a96e46
Vincent Bitter <vincent@vincentbitter.nl>
2025-07-16 08:03:49 +0200

    refactor: Use own dashboard code instead of depending on core dashboard.php.

--- admin/page_dashboard.php
+++ admin/page_dashboard.php
@@ -3,21 +3,53 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-require_once(ABSPATH . 'wp-admin/includes/dashboard.php');
+function fcm_add_dashboard_widget($widget_id, $widget_name, $callback, $context = 'normal')
+{
+    $screen = get_current_screen();
+    add_meta_box($widget_id, $widget_name, $callback, $screen, $context);
+}
+
+function fcm_dashboard()
+{
+    $screen = get_current_screen();
+?>
+    <div id="dashboard-widgets" class="metabox-holder">
+        <div id="postbox-container-1" class="postbox-container">
+            <?php do_meta_boxes($screen->id, 'normal', ''); ?>
+        </div>
+        <div id="postbox-container-2" class="postbox-container">
+            <?php do_meta_boxes($screen->id, 'side', ''); ?>
+        </div>
+        <div id="postbox-container-3" class="postbox-container">
+            <?php do_meta_boxes($screen->id, 'column3', ''); ?>
+        </div>
+        <div id="postbox-container-4" class="postbox-container">
+            <?php do_meta_boxes($screen->id, 'column4', ''); ?>
+        </div>
+    </div>
+
+    <?php
+    wp_nonce_field('closedpostboxes', 'closedpostboxesnonce', false);
+    wp_nonce_field('meta-box-order', 'meta-box-order-nonce', false);
+}
+
+function fcm_dashboard_setup()
+{
+    if (is_blog_admin() && current_user_can('edit_posts')) {
+        fcm_add_dashboard_widget('fcm_dashboard_right_now', __('At a Glance', 'football-club-manager'), 'fcm_dashboard_right_now');
+    }
+}
 
 if (! function_exists('fcm_page_dashboard')) {
     function fcm_page_dashboard()
     {
-        if (is_blog_admin() && current_user_can('edit_posts')) {
-            wp_add_dashboard_widget('fcm_dashboard_right_now', __('At a Glance', 'football-club-manager'), 'fcm_dashboard_right_now');
-        }
-
+        fcm_dashboard_setup();
         wp_enqueue_script('dashboard');
-?>
+    ?>
         <div class="wrap">
             <h1>Football Club Manager</h1>
             <div id="dashboard-widgets-wrap">
-                <?php wp_dashboard(); ?>
+                <?php fcm_dashboard(); ?>
             </div><!-- dashboard-widgets-wrap -->
         </div>
     <?php
