4cfbf858e81bb663a09404f7163530bfe2af7733
Vincent Bitter <5149862+vincentbitter@users.noreply.github.com>
2025-11-19 20:07:21 +0100

    feat: Schedule block showing upcoming matches.

---
+++ blocks/schedule/edit.js
@@ -0,0 +1,121 @@
+import { __ } from "@wordpress/i18n";
+import { useSelect } from "@wordpress/data";
+import { useDebounce } from "@wordpress/compose";
+import { useState, useEffect } from "@wordpress/element";
+import { PanelBody, RangeControl } from "@wordpress/components";
+import { InspectorControls, useBlockProps } from "@wordpress/block-editor";
+
+export default function Edit({ attributes, setAttributes }) {
+	const { numberOfItems, numberOfDays } = attributes;
+
+	const [params, setParams] = useState({ numberOfDays, numberOfItems });
+	const debouncedUpdate = useDebounce((days, items) => {
+		setParams({ numberOfDays: days, numberOfItems: items });
+	}, 300);
+
+	useEffect(() => {
+		debouncedUpdate(numberOfDays, numberOfItems);
+	}, [numberOfDays, numberOfItems]);
+
+	const teams = useSelect(
+		(select) =>
+			select("core").getEntityRecords("postType", "fcmanager_team", {
+				per_page: -1,
+			}),
+		[],
+	);
+
+	const matches = useSelect(
+		(select) =>
+			select("core")
+				.getEntityRecords("postType", "fcmanager_match", {
+					per_page: params.numberOfItems,
+					meta_key: "_fcmanager_match_date",
+					meta_value: new Date(
+						new Date().getTime() + params.numberOfDays * 24 * 60 * 60 * 1000,
+					)
+						.toISOString()
+						.slice(0, 10),
+					meta_compare: "<",
+					meta_type: "DATE",
+					upcoming: true,
+				})
+				?.map((match) => ({
+					id: match.id,
+					date: match.meta._fcmanager_match_date,
+					starttime: match.meta._fcmanager_match_starttime,
+					team: match.meta._fcmanager_match_team,
+					opponent: match.meta._fcmanager_match_opponent,
+					away: match.meta._fcmanager_match_away == "1",
+				})),
+		[params],
+	);
+
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody
+					title={__("Query", "football-club-manager")}
+					initialOpen={true}
+				>
+					<RangeControl
+						label={__("Number of items", "football-club-manager")}
+						value={numberOfItems}
+						onChange={(newValue) =>
+							setAttributes({ numberOfItems: parseInt(newValue) })
+						}
+						help={__(
+							"Select the maximum number of matches to show.",
+							"football-club-manager",
+						)}
+					/>
+					<RangeControl
+						label={__("Number of days", "football-club-manager")}
+						value={numberOfDays}
+						onChange={(newValue) =>
+							setAttributes({ numberOfDays: parseInt(newValue) })
+						}
+						help={__(
+							"Select the number of days to search for matches.",
+							"football-club-manager",
+						)}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div {...useBlockProps()}>
+				<div class="fcmanager-schedule">
+					<h2>{__("Upcoming matches", "football-club-manager")}</h2>
+					{!matches?.length ? (
+						<p>{__("No matches found.", "football-club-manager")}</p>
+					) : (
+						<table class="fcmanager-matches">
+							<tbody>
+								{matches
+									?.filter((match) => teams?.find((t) => t.id == match.team))
+									.map((match) => (
+										<tr key={match.id}>
+											<td class="fcmanager-match-date">{match.date}</td>
+											<td class="fcmanager-match-time">{match.starttime}</td>
+											<td class="fcmanager-match-hometeam">
+												{match.away
+													? match.opponent
+													: teams?.find((t) => t.id == match.team)?.title
+															.rendered}
+											</td>
+											<td class="fcmanager-match-separator">-</td>
+											<td class="fcmanager-match-awayteam">
+												{match.away
+													? teams?.find((t) => t.id == match.team)?.title
+															.rendered
+													: match.opponent}
+											</td>
+										</tr>
+									))}
+							</tbody>
+						</table>
+					)}
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ blocks/schedule/render.php
@@ -0,0 +1,98 @@
+<?php
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+function fcmanager_render_schedule_block($attributes, $content)
+{
+    $teams = fcmanager_get_teams();
+
+    $numberOfItems = isset($attributes['numberOfItems']) && $attributes['numberOfItems'] > 0
+        ? intval($attributes['numberOfItems'])
+        : 20;
+
+    $numberOfDays = isset($attributes['numberOfDays']) && $attributes['numberOfDays'] > 0
+        ? intval($attributes['numberOfDays'])
+        : 14;
+
+    $matches = get_posts([
+        'post_type' => 'fcmanager_match',
+        'meta_query' => [
+            'date_min' => [
+                'key' => '_fcmanager_match_date',
+                'value' => wp_date('Y-m-d'),
+                'compare' => '>=',
+                'type' => 'DATE',
+            ],
+            'date_max' => [
+                'key' => '_fcmanager_match_date',
+                'value' => wp_date('Y-m-d', strtotime("+{$numberOfDays} days")),
+                'compare' => '<',
+                'type' => 'DATE',
+            ],
+            'goals_for_not_exists' => [
+                'key'     => '_fcmanager_match_goals_for',
+                'compare' => 'NOT EXISTS',
+            ],
+            'match_starttime' => [
+                'key'  => '_fcmanager_match_starttime',
+                'type' => 'TIME',
+            ],
+        ],
+        'orderby' => [
+            'date_min'   => 'ASC',
+            'match_starttime' => 'ASC',
+        ],
+        'numberposts' => $numberOfItems
+    ]);
+
+    ob_start();
+?>
+    <div class="fcmanager-schedule">
+        <h2><?php esc_html_e("Upcoming matches", "football-club-manager") ?></h2>
+        <?php if ($matches): ?>
+            <table class="fcmanager-matches">
+                <tbody>
+                    <?php foreach ($matches as $match): ?>
+                        <?php
+                        $away = get_post_meta($match->ID, '_fcmanager_match_away', true);
+                        $opponent = get_post_meta($match->ID, '_fcmanager_match_opponent', true);
+                        $team_obj = array_find($teams, function ($team) use ($match) {
+                            return $team->ID == get_post_meta($match->ID, '_fcmanager_match_team', true);
+                        });
+                        if (!$team_obj) continue;
+                        $team_name = $team_obj->post_title;
+                        ?>
+                        <tr>
+                            <td class="fcmanager-match-date">
+                                <?php echo esc_html(get_post_meta($match->ID, '_fcmanager_match_date', true)); ?>
+                            </td>
+                            <td class="fcmanager-match-time">
+                                <?php echo esc_html(get_post_meta($match->ID, '_fcmanager_match_starttime', true)); ?>
+                            </td>
+                            <td class="fcmanager-match-hometeam">
+                                <?php
+                                echo $away ? esc_html($opponent) : esc_html($team_name);
+                                ?>
+                            </td>
+                            <td class="fcmanager-match-separator">-</td>
+                            <td class="fcmanager-match-awayteam">
+                                <?php
+                                echo $away ? esc_html($team_name) : esc_html($opponent);
+                                ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+            </table>
+        <?php else: ?>
+            <p><?php esc_html_e('No matches found.', 'football-club-manager'); ?></p>
+        <?php endif; ?>
+    </div>
+<?php
+    return ob_get_clean();
+}
+
+register_block_type(__DIR__, [
+    'render_callback' => 'fcmanager_render_schedule_block',
+]);
